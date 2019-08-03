b981416eb242f954e8d782da84a5a3ba9018bbb0
Hakob <kyurkchyan.akop@gmail.com>
2019-08-03 17:59:29 +0400

    events in progres

--- src/views/ABEvents/Events.js
+++ src/views/ABEvents/Events.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react';
+import {events} from './data';
+import {Row, Table, FormGroup,Input,Label,Button } from 'reactstrap';
+import moment from 'moment';
 
 class Events extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            events :events
+            
+        };
+    };
+   
     render() {
         return (
             <div className="animated fadeIn">
-                <div className="card">
-                    <div className="card-header">
-                        Events
-                    </div>
-                    <div className="card-body">
-                        <p>Coming Soon</p>
-                        <table className="table">
-                            <thead>
+                <Row>
+                    <Table responsive>
+                        <thead>
                             <tr>
-                                <th>column 1</th>
-                                <th>column 2</th>
+                                <th>
+                                    <FormGroup check className="checkbox">
+                                        <Input 
+                                        className="form-check-input" 
+                                        type="checkbox" 
+                                        id="checkbox1" 
+                                        name="checkbox1" 
+                                        value="option1"
+                                        />
+                                        <Label check className="form-check-label" htmlFor="checkbox1"></Label>
+                                    </FormGroup>
+                                </th>
+                                <th>id</th>
+                                <th>Name</th>
+                                <th>Discription</th>
+                                <th>Label</th>
+                                <th>creation <br/>data</th>
+                                <th>Header<br/> tamblet</th>
                             </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>
-                                    test
-                                </td>
-                                <td>row</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                        </thead>
+                        <tbody>
+                            {this.state.events.map(function(el){
+                                return (
+                                    <tr key={el.id.toString()}>
+                                        <td>
+                                            <FormGroup check className="checkbox">
+                                                <Input 
+                                                className="form-check-input" 
+                                                type="checkbox" 
+                                                id="checkbox1" 
+                                                name="checkbox1" 
+                                                value="option1" />
+                                                <Label check className="form-check-label" htmlFor="checkbox1"></Label>
+                                            </FormGroup>
+                                        </td>
+                                        <td>{el.id}</td>
+                                        <td>{el.name}</td>
+                                        <td dangerouslySetInnerHTML={{__html: el.description}}/>
+                                        <td dangerouslySetInnerHTML={{__html: el.label}}/>
+                                        <td>{moment(el.creationDate).format("DD.MM.YYYY")}</td>
+                                        <td>
+                                            <Button active block color="primary" aria-pressed="true">
+                                            SHOW<br/>
+                                            HEADER<br/>
+                                            PREVIEW
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                )
+                                })
+                                }
+                        </tbody>
+                    </Table>
+                </Row>
             </div>
         );
     }
---
+++ src/views/ABEvents/data.js
@@ -0,0 +1,382 @@
+export const events = [
+    {
+      "id": 11,
+      "name": "Test",
+      "headerTemplate": "[{\"type\":\"companyDescription\",\"text\":\"description text\",\"title\":true},{\"type\":\"companyContacts\",\"title\":\"phone\",\"text\":\"+3747777777777\"}]",
+      "description": "<p><span style=\"background-color: rgba(0, 0, 0, 0.07); color: rgba(0, 0, 0, 0.87);\">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac tincidunt massa. Integer porttitor libero a lorem condimentum dignissim. Cras ultrices vulputate laoreet. Sed id vestibulum elit, nec sodales metus. Integer quis ex non neque rhoncus mollis ac et nibh. Donec scelerisque elit pellentesque dignissim varius. Suspendisse vitae ante ut massa tincidunt malesuada. Suspendisse placerat libero a dui vulputate, at lacinia sem blandit. Curabitur diam quam, efficitur et mi consequat, imperdiet maximus nibh. Aliquam quam odio, convallis feugiat pharetra vel, vehicula in arcu. Suspendisse tempus, dui non tristique scelerisque, arcu metus fringilla justo, sed luctus felis ante vel tellus. Nam nulla eros, malesuada nec auctor in, cursus nec turpis. Nam id dapibus neque, nec sagittis lacus. Cras ultricies, metus ac iaculis volutpat, libero justo congue orci, at vulputate massa mi ac ante. Duis rutrum in dui quis dapibus.</span></p>",
+      "label": "<p>test label</p>",
+      "cases": [],
+      "audios": [],
+      "documents": [],
+      "creationDate": "2019-07-31T00:00:00",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 10,
+      "name": "",
+      "headerTemplate": "[]",
+      "description": "",
+      "label": "",
+      "cases": [],
+      "audios": [],
+      "documents": [],
+      "creationDate": "2019-07-31T17:50:37.42",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 9,
+      "name": null,
+      "headerTemplate": null,
+      "description": null,
+      "label": null,
+      "cases": [],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:00:00",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 8,
+      "name": "new event",
+      "headerTemplate": "[{\"type\":\"companyDescription\",\"text\":\"description text\"},{\"type\":\"companyContacts\",\"title\":\"contact title\",\"text\":\"contact\"}]",
+      "description": null,
+      "label": null,
+      "cases": [
+        {
+          "id": 11,
+          "user": {
+            "id": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+            "firstname": "Ololoev",
+            "lastname": "Ololo"
+          },
+          "userId": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+          "eventId": 8,
+          "name": "New case",
+          "coordinates": {
+            "latitude": 55.628417678428725,
+            "longtude": 37.640790939331062
+          },
+          "tags": [],
+          "tagIds": [],
+          "images": []
+        }
+      ],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:00:00",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 7,
+      "name": "Pro version Features",
+      "headerTemplate": "[{\"type\":\"companyDescription\",\"text\":\"ALIBIAPPS.COM\",\"title\":true},{\"type\":\"companyDescription\",\"text\":\"Event confirm service\"},{\"type\":\"companyContacts\",\"title\":\"Facebook\",\"text\":\"mafmaster\"},{\"type\":\"companyContacts\",\"title\":\"e-mail\",\"text\":\"alibi@alibi.lv\"}]",
+      "description": null,
+      "label": null,
+      "cases": [
+        {
+          "id": 10,
+          "user": {
+            "id": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+            "firstname": "Ololoev",
+            "lastname": "Ololo"
+          },
+          "userId": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+          "eventId": 7,
+          "name": "New case",
+          "coordinates": {
+            "latitude": 55.75222,
+            "longtude": 37.61556
+          },
+          "tags": [
+            {
+              "id": 2,
+              "text": "Bad"
+            },
+            {
+              "id": 3,
+              "text": "Good"
+            }
+          ],
+          "tagIds": [
+            2,
+            3
+          ],
+          "images": []
+        }
+      ],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:00:00",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 6,
+      "name": "Тестовый кейс",
+      "headerTemplate": null,
+      "description": null,
+      "label": null,
+      "cases": [
+        {
+          "id": 8,
+          "user": {
+            "id": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+            "firstname": "Ololoev",
+            "lastname": "Ololo"
+          },
+          "userId": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+          "eventId": 6,
+          "name": "Тестовый кейс111",
+          "coordinates": {
+            "latitude": 62.642709446905307,
+            "longtude": 7.0216651260852823
+          },
+          "tags": [
+            {
+              "id": 1,
+              "text": "Sales"
+            },
+            {
+              "id": 4,
+              "text": "Normal"
+            }
+          ],
+          "tagIds": [
+            1,
+            4
+          ],
+          "images": [
+            {
+              "id": 3,
+              "caseId": 8,
+              "url": "/images/53677044_405191563368121_2876189321520480256_n.jpg"
+            },
+            {
+              "id": 4,
+              "caseId": 8,
+              "url": "/images/gettyimages-150955680-1024x1024.jpg"
+            },
+            {
+              "id": 5,
+              "caseId": 8,
+              "url": "/images/_sNcoqoSCgs.jpg"
+            }
+          ]
+        },
+        {
+          "id": 9,
+          "user": {
+            "id": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+            "firstname": "Ololoev",
+            "lastname": "Ololo"
+          },
+          "userId": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+          "eventId": 6,
+          "name": "New case",
+          "coordinates": {
+            "latitude": 55.75222,
+            "longtude": 37.61556
+          },
+          "tags": [],
+          "tagIds": [],
+          "images": []
+        }
+      ],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:00:00",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 5,
+      "name": "new event",
+      "headerTemplate": "[]",
+      "description": null,
+      "label": null,
+      "cases": [
+        {
+          "id": 7,
+          "user": {
+            "id": "73e838ea-c33e-4442-9aed-35747b8657de",
+            "firstname": "Viktor",
+            "lastname": "Shabaev"
+          },
+          "userId": "73e838ea-c33e-4442-9aed-35747b8657de",
+          "eventId": 5,
+          "name": "New case 3",
+          "coordinates": {
+            "latitude": 55.75222,
+            "longtude": 37.61556
+          },
+          "tags": [
+            {
+              "id": 1,
+              "text": "Sales"
+            },
+            {
+              "id": 4,
+              "text": "Normal"
+            }
+          ],
+          "tagIds": [
+            1,
+            4
+          ],
+          "images": []
+        }
+      ],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:00:00",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 4,
+      "name": "new event",
+      "headerTemplate": "[]",
+      "description": null,
+      "label": null,
+      "cases": [
+        {
+          "id": 6,
+          "user": {
+            "id": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+            "firstname": "Ololoev",
+            "lastname": "Ololo"
+          },
+          "userId": "fbc7835f-c402-4d93-abd6-13de4743ca09",
+          "eventId": 4,
+          "name": "New case",
+          "coordinates": {
+            "latitude": 57.04549248022002,
+            "longtude": 36.293334960937507
+          },
+          "tags": [],
+          "tagIds": [],
+          "images": []
+        }
+      ],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:06:34",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    },
+    {
+      "id": 3,
+      "name": "Тестовый кейс",
+      "headerTemplate": "[{\"type\":\"companyDescription\",\"text\":\"Alibi\",\"title\":true},{\"type\":\"companyContacts\",\"title\":\"envelope\",\"text\":\"email@email.ru\"}]",
+      "description": "<p>&nbsp;</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac tincidunt massa. Integer porttitor libero a lorem condimentum dignissim. Cras ultrices vulputate laoreet. Sed id vestibulum elit, nec sodales metus. Integer quis ex non neque rhoncus mollis ac et nibh. Donec scelerisque elit pellentesque dignissim varius. Suspendisse vitae ante ut massa tincidunt malesuada. Suspendisse placerat libero a dui vulputate, at lacinia sem blandit. Curabitur diam quam, efficitur et mi consequat, imperdiet maximus nibh. Aliquam quam odio, convallis feugiat pharetra vel, vehicula in arcu. Suspendisse tempus, dui non tristique scelerisque, arcu metus fringilla justo, sed luctus felis ante vel tellus. Nam nulla eros, malesuada nec auctor in, cursus nec turpis. Nam id dapibus neque, nec sagittis lacus. Cras ultricies, metus ac iaculis volutpat, libero justo congue orci, at vulputate massa mi ac ante. Duis rutrum in dui quis dapibus.</p><p>Proin egestas elit ac nulla varius rhoncus. Cras et justo sit amet lectus aliquam dignissim. Cras accumsan est dapibus, pretium ex eu, fermentum ipsum. Vivamus dignissim nisl ante, ac hendrerit diam feugiat et. Nulla posuere tortor lacinia tempus bibendum. Maecenas sed nulla dolor. Nullam eleifend nisi id metus aliquet, vel malesuada eros bibendum. Morbi semper turpis at odio molestie, quis suscipit magna egestas. Morbi varius nibh nibh, et semper tellus dapibus eget. Nullam pharetra in quam at porta. Fusce consequat purus et eros consequat convallis. Morbi pretium quam sit amet ante tristique luctus. Etiam eu mattis lorem. Etiam varius pretium mauris, vitae viverra erat. Donec iaculis ex id ex fringilla, tincidunt volutpat.</p>",
+      "label": "<p>With respect&nbsp;</p>",
+      "cases": [
+        {
+          "id": 4,
+          "user": {
+            "id": "73e838ea-c33e-4442-9aed-35747b8657de",
+            "firstname": "Viktor",
+            "lastname": "Shabaev"
+          },
+          "userId": "73e838ea-c33e-4442-9aed-35747b8657de",
+          "eventId": 3,
+          "name": "Тестовый кейс #1",
+          "coordinates": {
+            "latitude": 55.75222,
+            "longtude": 37.61556
+          },
+          "tags": [
+            {
+              "id": 1,
+              "text": "Sales"
+            },
+            {
+              "id": 3,
+              "text": "Good"
+            }
+          ],
+          "tagIds": [
+            1,
+            3
+          ],
+          "images": [
+            {
+              "id": 11,
+              "caseId": 4,
+              "url": "/images/32d11ffd-bc53-41bb-ad53-27499244bf36.png"
+            },
+            {
+              "id": 7,
+              "caseId": 4,
+              "url": "/images/TexturesCom_WoodPlanksOld0292_4_seamless_S.jpg"
+            },
+            {
+              "id": 9,
+              "caseId": 4,
+              "url": "/images/949839c4-1a3e-494c-bcab-8e00d6f3372a.jpg"
+            },
+            {
+              "id": 10,
+              "caseId": 4,
+              "url": "/images/1f4f1810-c83c-40d3-9113-9c4504d790f4.png"
+            },
+            {
+              "id": 1,
+              "caseId": 4,
+              "url": "/images/Wolfenstein_3D_Screenshot_2019.04.17_-_12.42.58.10.png"
+            },
+            {
+              "id": 6,
+              "caseId": 4,
+              "url": "/images/google-plus.png"
+            }
+          ]
+        },
+        {
+          "id": 5,
+          "user": {
+            "id": "73e838ea-c33e-4442-9aed-35747b8657de",
+            "firstname": "Viktor",
+            "lastname": "Shabaev"
+          },
+          "userId": "73e838ea-c33e-4442-9aed-35747b8657de",
+          "eventId": 3,
+          "name": "Тестовый кейс #2",
+          "coordinates": {
+            "latitude": 55.812418476413818,
+            "longtude": 38.645246028900154
+          },
+          "tags": [
+            {
+              "id": 1,
+              "text": "Sales"
+            },
+            {
+              "id": 3,
+              "text": "Good"
+            }
+          ],
+          "tagIds": [
+            1,
+            3
+          ],
+          "images": [
+            {
+              "id": 2,
+              "caseId": 5,
+              "url": "/images/189e4ada-d1d9-4daa-8c60-82d75e693c4c.png"
+            }
+          ]
+        }
+      ],
+      "audios": [],
+      "documents": [],
+      "creationDate": "0001-01-01T00:00:51",
+      "downloadDate": "0001-01-01T00:00:00",
+      "editTime": "0001-01-01T00:00:00"
+    }
+  ];
