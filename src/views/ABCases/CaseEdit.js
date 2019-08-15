bbd44b20bb0d5e1d787f74b08fb96d8687948722
Hakob <kyurkchyan.akop@gmail.com>
2019-08-15 20:22:59 +0400

    make new component CaseEdit

--- src/views/ABCases/CaseEdit.js
+++ src/views/ABCases/CaseEdit.js
@@ -1,35 +1,81 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import FilledInput from "@material-ui/core/FilledInput";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import NativeSelect from "@material-ui/core/NativeSelect";
+import {api} from './../../services/API';
 
 
 class CaseEdit extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            users:[],
+            tags:[],
+        }   
+    };
+    componentDidMount(){
+        api.get('/users').then(res => {
+            const users = res.data;
+            this.setState({users});
+          });
+          api.get('/tags').then(res => {
+            const tags = res.data;
+            this.setState({tags});
+          });
+    };
+   handleChange = name => event => {
+        this.setState({
+          ...this.state,
+          [name]: event.target.value
+        });
+      };
     render(){
         return(
             <div>
-                  <FormControl className='classesFormControl'>
-        <InputLabel htmlFor="age-native-simple">Age</InputLabel>
-        <Select
-          native
-          value='adadad'
-          inputProps={{
-            name: "aaa",
-            id: "age-native-simple"
-          }}
-        >
-          <option value="" />
-          <option value={10}>Ten</option>
-          <option value={20}>Twenty</option>
-          <option value={30}>Thirty</option>
-        </Select>
-      </FormControl>
+                <div>
+                    <TextField id="standard-name"/>
+                </div>
+                <div>
+                    <FormControl className='classesFormControl'>
+                        <InputLabel htmlFor="age-native-simple">User</InputLabel>
+                        <Select
+                            native
+                            value={this.state.users.lastname}
+                        onChange={this.handleChange()}
+                    > 
+                    <option value=""/>
+                        {this.state.users.map((el) => {
+                        return (
+                            <option key={el.id.toString()}>{el.lastname}</option>
+                        )})}
+                    </Select>
+                </FormControl>
+                </div>
+                <div>
+                <FormControl className='classesFormControl'>
+                    <InputLabel htmlFor="age-native-simple">Tags</InputLabel>
+                    <Select
+                        native
+                        value={this.state.tags.text}
+                        onChange={this.handleChange()}
+                    > 
+                    <option value=""/>
+                        {this.state.tags.map((el) => {
+                        return (
+                            <option key={el.id.toString()}>{el.text}</option>
+                        )})}
+                    </Select>
+                </FormControl>
+                </div>
+                <Button variant="contained" color="secondary" className='classesButton'>
+        Delete
+        
+      </Button>
             </div>
         )
     }
